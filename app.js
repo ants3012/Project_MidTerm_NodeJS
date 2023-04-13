0a0c27d65835b2ea7c0607108548a85185aa3882
ants3012 <thanhan7447@gmail.com>
2023-04-13 09:52:40 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const handlebars = require('express-handlebars');
+const path = require('path');
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// template engine
+app.engine('hbs', handlebars.engine({
+  defaultLayout : 'main',
+  extname : '.hbs'
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
